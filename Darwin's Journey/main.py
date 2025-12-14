8adfd6ae6545f526d51a8aac271d8171ff40af16
David Wang <david.wangruijia@gmail.com>
2025-12-14 01:58:47 -0500

    update on attribute

--- Darwin's Journey/main.py
+++ Darwin's Journey/main.py
@@ -1,3 +1,21 @@
 class GameManager(object):
     def __init__(self):
-        self.board = {"island1":[]}
+        self.board = {"island1":[],
+                      "island2":[],
+                      "island3":[],
+                      "sea":[],
+
+                      "wax":[],
+                      "fossil":[],
+                      "turn order":[],
+                      "achievement":[],
+                      "envelope":[],
+
+                      "green":[],
+                      "blue":[],
+                      "yellow":[],
+                      "red":[],
+                      "extra":[],
+                      "lens":[]
+                      }
+
