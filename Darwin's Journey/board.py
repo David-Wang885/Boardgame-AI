2f2595d462ccbb9d7f20c98b6255c715ffd97ed7
David Wang <david.wangruijia@gmail.com>
2025-12-16 21:58:08 -0500

    create board

---
+++ Darwin's Journey/board.py
@@ -0,0 +1,20 @@
+class Board(object):
+    def __init__(self):
+        self.board = {"island1":[],
+                      "island2":[],
+                      "island3":[],
+                      "sea":[],
+
+                      "wax":[],
+                      "fossil":[],
+                      "turn order":[],
+                      "achievement":[],
+                      "envelope":[],
+
+                      "green":[],
+                      "blue":[],
+                      "yellow":[],
+                      "red":[],
+                      "extra":[],
+                      "lens":[]
+                      }
--- Darwin's Journey/main.py
+++ Darwin's Journey/main.py
@@ -1,21 +1,6 @@
+from board import *
+
 class GameManager(object):
     def __init__(self):
-        self.board = {"island1":[],
-                      "island2":[],
-                      "island3":[],
-                      "sea":[],
-
-                      "wax":[],
-                      "fossil":[],
-                      "turn order":[],
-                      "achievement":[],
-                      "envelope":[],
-
-                      "green":[],
-                      "blue":[],
-                      "yellow":[],
-                      "red":[],
-                      "extra":[],
-                      "lens":[]
-                      }
+        self.board = Board
 
